9ca0e33e440f1f9909e1358c9934a8a7b6332c0a
ramon cantanhede gusmão <ramoncgusmao@gmail.com>
2019-11-03 23:10:21 -0300

    commit inicial, começando o projeto

---
+++ src/main/java/com/ramon/gerenciadorfinancas/GerenciadorfinancasApplication.java
@@ -0,0 +1,13 @@
+package com.ramon.gerenciadorfinancas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorfinancasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorfinancasApplication.class, args);
+	}
+
+}
